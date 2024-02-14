2e89350c71f938a2aea5e4c062576d3f3dc270e9
Arthur Amorim <aamorim6425@gmail.com>
2024-02-13 21:28:48 -0300

    Initial Commit

---
+++ sistema-cliente-completo/src/Admnistrador.java
@@ -0,0 +1,24 @@
+
+public class Admnistrador extends Funcionario implements Autenticavel{
+
+	AutenticacaoUtil autenticador;
+	
+	public Admnistrador () {
+		this.autenticador = new AutenticacaoUtil();
+	}
+	
+	@Override
+	public double getBonificacao() {
+		return 50;
+	}
+
+	@Override
+	public void setSenha(int senha) {
+		this.autenticador.setSenha(senha);	
+	}
+
+	@Override
+	public boolean autentica(int senha) {
+		return this.autenticador.autentica(senha);
+}
+}
---
+++ sistema-cliente-completo/src/Autenticavel.java
@@ -0,0 +1,11 @@
+
+// contrato autenticavel
+	// qm assina esse contrato, precisa implementar
+		// metodo setSenha
+			//metoro autentica
+public abstract interface Autenticavel {
+	
+	public abstract void setSenha(int senha);
+	
+	public abstract boolean autentica(int senha);
+}
